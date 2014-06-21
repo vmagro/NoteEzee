08b19d7dd9e0425096e2a78a10def9a00cd4dd82
Vinnie Magro <vmagro@socaldevs.com>
2014-06-21 06:25:33 -0700

    I'm so tired I don't even know what I did

--- src/assets/linker/js/lecture.js
+++ src/assets/linker/js/lecture.js
@@ -11,4 +11,29 @@ $(function(){
     window.location.href = url;
     console.log('click');
   });
+
+  $('.play-pause-button').on('click', function(){
+    var currentBg = $(this).css('background-image');
+    var toPlay = false;
+    if(currentBg.indexOf('Play') != -1){
+      toPlay = true;
+      $(this).css('background-image', 'url(/linker/images/PauseBT.png)');
+    } else {
+      toPlay = false;
+      $(this).css('background-image', 'url(/linker/images/PlayBT.png)');
+    }
+    if(toPlay){
+      $('audio')[0].play();
+    } else {
+      $('audio')[0].pause();
+    }
+  });
+
+  $('audio')[0].addEventListener('durationchange', function(){
+    $('.total-progress').attr('max', $('audio')[0].duration);
+  });
+
+  $('audio')[0].addEventListener('timeupdate', function(){
+    $('.total-progress').attr('value', $('audio')[0].currentTime);
+  });
 });
